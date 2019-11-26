26f7071b55aafc2b3f2c9dd7d3983747d103958a
Pegasids <tongtongcanopus@gmail.com>
2019-11-25 23:29:56 -0800

    Update Tumor classification.py

--- Tumor_classification/Tumor classification.py
+++ Tumor_classification/Tumor classification.py
@@ -15,7 +15,7 @@ y = cancer.target
 x_train, x_test, y_train, y_test = sklearn.model_selection.train_test_split(x, y, test_size=0.2)
 
 
-clf = svm.SVC(kernel="linear")
+clf = svm.SVC(kernel="linear", C=2)
 clf.fit(x_train, y_train)
 
 y_pred = clf.predict(x_test)
